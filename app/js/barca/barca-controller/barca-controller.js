2f7ef045a79b9acf574082a302818a1810cf1fee
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-06-19 16:37:23 -0700

    wrote barca-contoller

---
+++ app/js/barca/barca-controller/barca-controller.js
@@ -0,0 +1,52 @@
+'use strict';
+
+module.exports = function(app) {
+  app.controller('BarcaController', ['$http', BarcaController]);
+
+  function BarcaController($http) {
+    this.$http = $http;
+    this.barcas = [];
+  }
+
+  BarcaController.prototype.getBarcas = function() {
+    this.$http.get('http://localhost:6969/barca')
+    .then((res) => {
+      this.barcas = res.data;
+    }, (err) => {
+      console.log(err);
+    });
+  };
+
+  BarcaController.prototype.addBarca = function() {
+    this.$http.post('http://localhost:6969/barca', this.newBarca)
+    .then((res) => {
+      this.barcas.push(res.data);
+      this.newBarca = null;
+    }, (err) => {
+      console.log(err);
+    });
+  };
+
+  BarcaController.prototype.deleteBarca = function(barca) {
+    this.$http.delete('http://localhost:6969/barca/' + barca._id)
+    .then(() => {
+      let index = this.barcas.indexOf(barca);
+      this.barcas.splice(index, 1);
+    }, (err) => {
+      console.log(err);
+    });
+  };
+
+  BarcaController.prototype.updateBarca = function(barca, updateBarca) {
+    barca.name = updateBarca.name;
+
+    this.$http.put('http://localocalhost:6969/barca/', barca)
+    .then(() => {
+      this.barcas = this.barcas.map(n => {
+        return n._id === barca._id ? barca : n;
+      });
+    }, (err) => {
+      console.log(err);
+    });
+  };
+};
---
+++ app/js/manUnited/manUnited.js
@@ -0,0 +1,3 @@
+module.exports = function(app) {
+  require('./manUnited-controller/manUnited-controller')(app);
+};
