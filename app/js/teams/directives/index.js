5cb2406f81fc2267418696c6e693f81460bde803
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-06-29 11:59:49 -0700

    made the functionality for barca with directives work

--- app/js/teams/directives/index.js
+++ app/js/teams/directives/index.js
@@ -3,4 +3,6 @@ module.exports = function(app) {
   require('./barca_form')(app);
   require('./barca_list')(app);
   require('./man_united_list')(app);
+  require('./barca_new_player_form')(app);
+    require('./man_united_new_player_form')(app);
 };
