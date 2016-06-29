024febee207bd046965e8a037b94955a2818baaa
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-06-29 14:31:50 -0700

    using one form now

--- app/js/teams/directives/barca_new_player_form.js
+++ app/js/teams/directives/barca_new_player_form.js
@@ -1,18 +1,34 @@
+'use strict';
 module.exports = function(app) {
   app.directive('barcaNewPlayerForm', function() {
     return {
       scope: {
         type: '@',
-        player: '='
+        player: '=',
+        team: '@'
       },
       templateUrl: './templates/teams/barca_new_player_form.html',
       require: '^^ngController',
       link: function($scope, elem, attr, controller) {
         console.log(controller);
-        $scope.deleteBarca = controller.deleteBarca;
-        $scope.updateBarca = controller.updateBarca;
-        $scope.submit = $scope.type === 'new' ? controller.addBarca
-        : controller.updateBarca;
+
+        let configMethods = {
+          barca: function($scope) {
+            $scope.delete = controller.deleteBarca;
+            $scope.update = controller.updateBarca;
+            $scope.submit = $scope.type === 'new' ? controller.addBarca
+            : controller.updateBarca;
+            $scope.formMessage = $scope.type === 'new' ? 'Add new Barca player' : 'Update player';
+          },
+          manUnited: function($scope) {
+            $scope.delete = controller.deletemanUnited;
+            $scope.update = controller.updatemanUnited;
+            $scope.submit = $scope.type === 'new' ? controller.addmanUnited
+            : controller.updatemanUnited;
+            $scope.formMessage = $scope.type === 'new' ? 'Add new Man United player' : 'Update player';
+          }
+        };
+        configMethods[$scope.team]($scope);
       }
     };
   });
