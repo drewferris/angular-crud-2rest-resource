f9e87812204101fa7f1ee3264379b02836e2d073
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-06-30 10:43:04 -0700

    removed unnecessary files

--- app/js/teams/directives/index.js
+++ app/js/teams/directives/index.js
@@ -1,8 +1,5 @@
 module.exports = function(app) {
-  require('./man_united_form')(app);
-  require('./barca_form')(app);
   require('./barca_list')(app);
   require('./man_united_list')(app);
   require('./player_form')(app);
-  require('./man_united_new_player_form')(app);
 };
