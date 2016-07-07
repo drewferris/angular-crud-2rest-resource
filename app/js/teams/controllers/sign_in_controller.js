97ab00d7a9cc5bbf27e74fb054269fd124ee530a
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-07-07 11:00:15 -0700

    wrote signin controller

--- app/js/teams/controllers/sign_in_controller.js
+++ app/js/teams/controllers/sign_in_controller.js
@@ -1,5 +1,26 @@
 'use strict';
 
 module.exports = function(app) {
-  
-}
+  app.controller('SigninController', function($location, AuthService) {
+    this.goHome = function() {
+      $location.url('/');
+    };
+
+    this.signUp = function(user) {
+      AuthService.signUp(user)
+        .then((res) => {
+          console.log(res);
+        })
+        .then((err) => {
+          console.log(err);
+        });
+    };
+
+    this.signIn = function(user) {
+      AuthService.signIn(user)
+        .then((res) => {
+          console.log(res);
+        });
+    };
+  });
+};
